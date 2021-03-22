74c8bcb9fbc4623d8c03016eb7b683e7970c0855
EvgeniyDEM <57892771+EvgeniyDEM@users.noreply.github.com>
2021-03-22 15:27:34 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE).then((cache) => {
             return cache.addAll([
-                '/img/background'
+                'index'
             ]);
         })
     );
