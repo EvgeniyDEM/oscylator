55c35eab0b392962ccb1c7e47835e9d83479a03b
EvgeniyDEM <57892771+EvgeniyDEM@users.noreply.github.com>
2021-03-23 13:41:52 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -34,3 +34,12 @@ self.addEventListener('activate', function (event) {
     })
   )
 })
+
+
+self.addEventListener('push', function(event) {
+  if (event.data) {
+    console.log('This push event has data: ', event.data.text());
+  } else {
+    console.log('This push event has no data.');
+  }
+});
