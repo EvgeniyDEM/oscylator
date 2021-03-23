4a9a228930d6c5ef72c66376ecbd7e604b3ebffa
EvgeniyDEM <57892771+EvgeniyDEM@users.noreply.github.com>
2021-03-23 14:03:36 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -39,8 +39,8 @@ self.addEventListener('activate', function (event) {
 self.addEventListener('push', function(event) {
   if (event.data) {
     //console.log('This push event has data: ', event.data.text());
-     //const promiseChain = self.registration.showNotification('Hello, World.');
-     self.registration.showNotification('Hello, World.');
+     const promiseChain = self.registration.showNotification('Hello, World.');
+    event.waitUntil(promiseChain);
   } else {
     console.log('This push event has no data.');
   }
