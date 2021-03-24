b335d4c70a648c5e9f3d41a271ac3fed5fc469a6
EvgeniyDEM <57892771+EvgeniyDEM@users.noreply.github.com>
2021-03-24 11:44:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./sw.js')
       .then(() => navigator.serviceWorker.ready.then((worker) => {
         worker.sync.register('syncdata');
-        function SubScribeNotification()
+        SubScribeNotification()
       }))
       .catch((err) => console.log(err));
 }
