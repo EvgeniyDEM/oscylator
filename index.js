47533f04b5831ea79933f5d71e63817a5e880d73
EvgeniyDEM <57892771+EvgeniyDEM@users.noreply.github.com>
2021-03-24 10:30:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./sw.js')
       .then(() => navigator.serviceWorker.ready.then((worker) => {
         worker.sync.register('syncdata');
+        
+        
       }))
       .catch((err) => console.log(err));
+}
+ Notification.requestPermission();
+var btnSubScribePush=document.querySelector('btnSubScribePush');
+btnSubScribePush.click=function(){
+    //if (Notification.permission === "granted"){
+    Notification.requestPermission();
 }
